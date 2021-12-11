e14505a43ebaf1f8d3ff1870973d15cd1117341f
Dominic Carullo <dominiccarullo@msn.com>
2021-12-11 13:43:31 -0700

    created Q and A jsx page

---
+++ public/client/src/components/QandA.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import App from '../index.jsx';
+
+class QandA extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <h2>Q and A</h2>
+    )
+  }
+}
+
+export default QandA;
