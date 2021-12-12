fc830f9de404f06949113e0148cc4f1bd6ebc90c
Jeff Fox <jeffaveryfox@gmail.com>
2021-12-11 22:00:30 -0600

    Implemented text-only Product Breakdown

--- public/client/src/components/RatingsAndRevs/RatingsAndReviews.jsx
+++ public/client/src/components/RatingsAndRevs/RatingsAndReviews.jsx
@@ -46,17 +46,45 @@ class RatingsAndReviews extends React.Component{
             "photos": [],
           }
         ]
+      },
+      metadata: {
+        "product_id": "2",
+        "ratings": {
+          2: 1,
+          3: 1,
+          4: 2,
+          // ...
+        },
+        "recommended": {
+          0: 5
+          // ...
+        },
+        "characteristics": {
+          "Size": {
+            "id": 14,
+            "value": "4.0000"
+          },
+          "Width": {
+            "id": 15,
+            "value": "3.5000"
+          },
+          "Comfort": {
+            "id": 16,
+            "value": "4.0000"
+          },
+          // ...
       }
     }
   }
+}
 
   render() {
     return (
       <div>
         <h3>Ratings and Reviews</h3>
         <Reviews reviews={this.state.reviews}/>
-        <RatingBreakdown reviews={this.state.reviews.results}/>
-        <ProductBreakdown reviews={this.state.reviews}/>
+        <RatingBreakdown reviews={this.state.reviews.results} metadata={this.state.metadata}/>
+        <ProductBreakdown reviews={this.state.reviews} metadata={this.state.metadata}/>
       </div>
     )
   }
--- public/client/src/components/RatingsAndRevs/productBreakdown.jsx
+++ public/client/src/components/RatingsAndRevs/productBreakdown.jsx
@@ -7,10 +7,23 @@ class ProductBreakdown extends React.Component {
   }
 
   render() {
+    let metadata = this.props.metadata
+    var chars = metadata.characteristics
     return (
       <div>
         <h5>Product Breakdown</h5>
-        <p>Comfort: Perfect</p>
+        {chars.Size &&
+        <p>Size: {chars.Size.value}</p>}
+        {chars.Width &&
+        <p>Width: {chars.Width.value}</p>}
+        {chars.Comfort &&
+        <p>Comfort: {chars.Comfort.value}</p>}
+        {chars.Quality &&
+        <p>Quality: {chars.Quality.value}</p>}
+        {chars.Length &&
+        <p>Length: {chars.Length.value}</p>}
+        {chars.Fit &&
+        <p>Fit: {chars.Fit.value}</p>}
       </div>
     )
   }
