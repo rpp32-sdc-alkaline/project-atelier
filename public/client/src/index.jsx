4a43c3934e36f9a941d7a2ed0a8a24bdecebb103
Dominic Carullo <dominiccarullo@msn.com>
2021-12-16 18:19:31 -0700

    adjusted file paths to build correctly, started work on API connection

--- public/client/src/index.jsx
+++ public/client/src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Overview from './components/Overview/Overview.jsx'
 import RatingsAndReviews from './components/RatingsAndRevs/RatingsAndReviews.jsx'
 import QandA from './components/QandA/QandA.jsx'
-import App from ''
+import App from './components/app.jsx'
 
 ReactDOM.render(<App />, document.getElementById('app'))
 
--- server/index.js
+++ server/index.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const path = require('path');
 const port = 3000;
-const app = express()
+const app = express();
+const axios = require('axios');
+const token = require('../config.js');
 
 app.use(express.static(path.join(__dirname, '..', 'public/client')))
 app.use(express.json())
+
+app.get('/API', (req, res) => {
+  console.log('token', token.TOKEN);
+  axios({
+    method: 'GET',
+    url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products',
+    headers: {
+      Authorization: token.TOKEN
+    }
+  })
+  .then ((res) => {
+    console.log('response', res);
+  })
+  .catch((error) => {
+    throw error;
+  })
+
+
+  })
 
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`)
