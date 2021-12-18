ae092b5ab42ffcbc4ac9fdd98a0c1eab0d0b0355
Dominic Carullo <dominiccarullo@msn.com>
2021-12-17 19:33:15 -0700

    git ignore config file properly

--- config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  TOKEN: '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'
-}
--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const port = 3000;
 const app = express();
 const axios = require('axios');
-const token = require('../config.js');
+const token = require('../public/client/dist/config.js');
 
 app.use(express.static(path.join(__dirname, '..', 'public/client')))
 app.use(express.json())
