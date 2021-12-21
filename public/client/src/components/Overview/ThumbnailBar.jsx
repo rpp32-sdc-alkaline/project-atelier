c0e34c06757f118ae81b74046b3e6d5009f76036
merwhite11 <meredith.white91@gmail.com>
2021-12-21 13:15:07 -0800

    Main image changing on style overlay click

--- public/client/src/components/Overview/ThumbnailBar.jsx
+++ public/client/src/components/Overview/ThumbnailBar.jsx
@@ -1,18 +1,47 @@
 import React from 'react'
 
-const ThumbnailBar = (props) => {
-  const format = {
-    height: 100,
-    width: 80,
-    padding: 10
+class ThumbnailBar extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      clicked: false,
+      selected: ''
+    }
+  this.selectThumbnail = this.selectThumbnail.bind(this)
   }
-  return (
-    <div class="thumbnail-bar">
-      {props.photos.map((photo) =>
-        <img style={format} src={photo.thumbnail_url}></img>
-      )}
-    </div>
-  )
+
+  selectThumbnail(photo) {
+    this.setState({
+      clicked: true,
+      selected: photo.url
+    })
+  this.props.changeThumbnail(photo)
+  }
+
+  render() {
+    // var format = {
+    //   height: 100,
+    //   width: 80,
+    //   padding: 10
+    // }
+    // var className = this.state.clicked ? 'click-state' : 'base-state'
+    var className;
+    return (
+      <div class="thumbnail-bar">
+        {this.props.photos.map((photo) => {
+          className = photo.url === this.state.selected ? 'click-state' : 'base-state'
+          return (
+            <img class={className} key={photo.url} onClick={(e)=>{this.selectThumbnail(photo.url)}}
+            // style={format}
+            src={photo.thumbnail_url}>
+            </img>
+          )
+         })
+        }
+     </div>
+    )
+  }
 }
 
 
