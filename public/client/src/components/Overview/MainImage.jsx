9bebcce8532ea72c5d219ada49d3e43fcf6c20c1
merwhite11 <meredith.white91@gmail.com>
2021-12-29 13:42:15 -0800

    All 4 image buttons working

--- public/client/src/components/Overview/MainImage.jsx
+++ public/client/src/components/Overview/MainImage.jsx
@@ -9,50 +9,18 @@ class MainImage extends React.Component {
       currentMain: '',
       photos: [],
       current: 0
-      // currentMain: props.photos[0].url
     }
-  this.changeThumbnail = this.changeThumbnail.bind(this)
+  // this.changeThumbnail = this.changeThumbnail.bind(this)
   this.nextSlide = this.nextSlide.bind(this)
   this.prevSlide = this.prevSlide.bind(this)
   }
 
-  changeThumbnail(photo) {
-    this.setState({
-      currentMain: photo
-    })
-  }
-
   nextSlide() {
-   var newCurrent = this.state.current === this.state.photos.length ? 0
-   : this.state.current + 1
-   this.setState({
-     current: newCurrent
-   })
-
+   this.props.mainImageNext()
   }
 
   prevSlide() {
-   var newCurrent = this.state.current === 0 ? this.state.photos.length
-   :this.state.current - 1
-   this.setState({
-     current: newCurrent
-   })
-  }
-
-  componentDidUpdate(prevProps) {
-    if(this.props.photos[0] !== prevProps.photos[0]) {
-      this.setState({
-        currentMain: this.props.photos[0].url,
-        photos: this.props.photos
-      })
-    }
-  }
-
-  componentDidMount() {
-    this.setState({
-      currentMain: this.props.photos[0].url,
-      photos: this.props.photos
-    })
+  this.props.mainImagePrev()
   }
 
   render() {
@@ -62,25 +30,19 @@ class MainImage extends React.Component {
       padding: 30
     }
     return (
-      <div className="wrapper">
-        {/* <img style={format} src={this.state.currentMain}/> */}
-        <section className="slider">
+      <div className="slider">
           <FaArrowAltCircleLeft className="left-arrow" onClick={this.prevSlide}/>
           <FaArrowAltCircleRight className="right-arrow" onClick={this.nextSlide} />
-        {this.state.photos.map((photo, index) => {
-          return (
-            <div className={index === this.state.current ? 'slide active' : 'slide'} key={index}>
-              {index === this.state.current && <img className="image" src={photo.url}/>}
-            </div>
-          )
-        })}
-        <ThumbnailBar changeThumbnail={this.changeThumbnail}
-        photos={this.props.photos} currentMain={this.state.currentMain}/>
-        </section>
+            <img style={format} src={this.props.image}/>
       </div>
     )
   }
-
 }
 
 export default MainImage;
+
+ // <div className={index === this.state.current ? 'slide active' : 'slide'} key={index}>
+            //   {index === this.state.current && <img className="image" src={photo.url}/>}
+            // </div>
+ {/* <ThumbnailBar changeThumbnail={this.changeThumbnail}
+        photos={this.props.photos} currentMain={this.state.currentMain}/> */}
--- public/client/src/components/Overview/ThumbnailBar.jsx
+++ public/client/src/components/Overview/ThumbnailBar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 class ThumbnailBar extends React.Component {
   constructor(props) {
     super(props)
-    var photos = [...this.props.photos, ...this.props.photos]
+    var photos = [...this.props.photos]
     this.state = {
       clicked: false,
       selected: '',
@@ -13,30 +13,38 @@ class ThumbnailBar extends React.Component {
     }
   this.selectThumbnail = this.selectThumbnail.bind(this)
   this.handleUpClick = this.handleUpClick.bind(this)
+  this.handleDownClick = this.handleDownClick.bind(this)
   }
 
-  selectThumbnail(photoUrl) {
-    // console.log('photo', photoUrl)
+  selectThumbnail(photoUrl, index) {
+    console.log('photo', index)
     this.setState({
       clicked: true,
       selected: photoUrl
     })
-  this.props.changeThumbnail(photoUrl)
+  this.props.changeThumbnail(photoUrl, index)
   }
 
   handleUpClick(e) {
-    const photos = this.state.photos.slice()
+    const photos = this.props.photos.slice()
     const index = photos.length - 1
     const photo = photos[index]
 
     photos.splice(index, 1)
     photos.unshift(photo)
 
-    this.setState({
-      photos: photos,
-      currentTopIndex: index
-    })
+    this.props.thumbnailScrollUp(photos)
   };
+
+  handleDownClick() {
+    const photos = this.props.photos.slice()
+    const photo = photos[0]
+
+    photos.splice(0, 1)
+    photos.push(photo)
+
+    this.props.thumbnailScrollDown(photos)
+  }
 
   // componentDidMount() {
   //     if (!this.state.scrolledPhotos) {
@@ -57,26 +65,23 @@ class ThumbnailBar extends React.Component {
     var downArrow;
     var index;
     var photos;
-    if (this.state.photos.length > 6) {
-      upArrow = <img className="up-arrow" onClick={this.handleUpClick} src="https://img.icons8.com/color/30/000000/circled-chevron-up--v1.png"/>
-      downArrow = <img className="down-arrow" src="https://img.icons8.com/color/30/000000/circled-chevron-down--v1.png"/>
+    if (this.state.photos.length > 5) {
+      upArrow = <img className="up-arrow" onClick={this.handleUpClick}
+      src="https://img.icons8.com/color/30/000000/circled-chevron-up--v1.png"/>
+      downArrow = <img className="down-arrow" onClick={this.handleDownClick}
+      src="https://img.icons8.com/color/30/000000/circled-chevron-down--v1.png"/>
     }
-    // if (!this.state.scrolledPhotos) {
-    //    photos = this.state.photos
-    // } else {
-    //   photos = this.state.scrolledPhotos
-    // }
     return (
       <div className="thumbnail-bar">
         {upArrow}
-        {this.state.photos.map((photo, index) => {
+        {this.props.photos.map((photo, index) => {
           // console.log('photo index', photo)
           // console.log('this.state.selected', this.state.selected)
           if (index < 7) {
             var className = photo.url === this.state.selected ? 'click-state' : 'base-state'
             return (
               <div key={index}>
-              <img className={className} onClick={(e)=>{this.selectThumbnail(photo.url)}}
+              <img className={className} onClick={(e)=>{this.selectThumbnail(photo.url, index)}}
               src={photo.thumbnail_url}>
               </img>
             </div>
